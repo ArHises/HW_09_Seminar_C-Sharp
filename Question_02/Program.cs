26519f2f6a0e1533a2e89a4475c6394056a7e4a1
Arhises <roma1039@gmail.com>
2023-02-25 22:04:16 +0200

    2nd q added

---
+++ Question_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int PrintTillN(int N, int M)
+{
+    if (N == M)
+        return N;
+    return PrintTillN(N - 1, M) + N;
+}
+
+Console.WriteLine("Enter 1st number: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Enter 2nd number: ");
+int M = int.Parse(Console.ReadLine()!);
+
+if (M < N)
+    System.Console.WriteLine(PrintTillN(N, M));
+else
+    System.Console.WriteLine(PrintTillN(M, N));;
