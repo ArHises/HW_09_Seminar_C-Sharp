809e8804dc2f15e25d0b295d0ae3c87e5059cafd
Arhises <roma1039@gmail.com>
2023-02-25 22:07:39 +0200

    3th q added

---
+++ Question_03/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int NumberSumTwo(int N)
+{
+    if (N / 10 == 0) return N;
+    return NumberSumTwo(N / 10) + N % 10;
+}
+
+Console.WriteLine("Enter number: ");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine(NumberSumTwo(N));
