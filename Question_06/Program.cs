fbb990c6a01806f1a82bd8c4bc0eff336034170b
Arhises <roma1039@gmail.com>
2023-02-28 20:08:47 +0200

    6 q added

---
+++ Question_06/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем.
+
+int SumSubsequence(int n, int first, int second)
+{
+    if (n == 1) return first + second;
+    int save = second;
+    second = first + second;
+    first = save;
+    Console.Write($"{second}, ");
+    return SumSubsequence(n - 1, first, second);
+}
+
+Console.Write("Enter first:");
+int first = int.Parse(Console.ReadLine()!);
+Console.Write("Enter second:");
+int second = int.Parse(Console.ReadLine()!);
+
+SumSubsequence(10, first, second);
