53a772070e201193a18d02f3abdf2a68e4094fc1
Arhises <roma1039@gmail.com>
2023-02-28 19:42:46 +0200

    5 q added

---
+++ Question_05/Program.cs
@@ -0,0 +1,13 @@
+﻿// Написать программу возведения числа А в целую степнь B 
+// B^A?
+int ASquerB(int a, int b)
+{
+    if(b == 0) return 1;
+    return ASquerB(a, b-1) * a;
+}
+Console.Write("Enter a:");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Enter b:");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(ASquerB(a,b));
